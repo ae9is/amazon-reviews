9fe1602f3c40ff0b95e96821112cf8110aec0b5f
ae9is <125031666+ae9is@users.noreply.github.com>
2024-05-10 23:11:45 -0700

    test: add year-specific tests for top reviewers endpoint

--- src/test/java/amazonrev/reviewer/ReviewerTests.java
+++ src/test/java/amazonrev/reviewer/ReviewerTests.java
@@ -44,23 +44,38 @@ public class ReviewerTests {
   void shouldGetTopReviewersByNumReviews() throws IOException {
     String graphqlDocument = "reviewer/topReviewers";
     String responseDocument = graphqlDocument + "ByNumReviews";
-    doTopReviewersTest(graphqlDocument, responseDocument, "NUM_REVIEWS", "OTk2MTcfMjQw");
+    doTopReviewersTest(graphqlDocument, responseDocument, "NUM_REVIEWS", "OTk2MTcfMjQw", null);
+  }
+
+  @Test
+  void shouldGetTopReviewersByNumReviewsFor2023() throws IOException {
+    String graphqlDocument = "reviewer/topReviewers";
+    String responseDocument = graphqlDocument + "ByNumReviews2023";
+    doTopReviewersTest(graphqlDocument, responseDocument, "NUM_REVIEWS", "MTIxODk2MR80NA==", 2023);
   }
 
   @Test
   void shouldGetTopReviewersByVotes() throws IOException {
     String graphqlDocument = "reviewer/topReviewers";
     String responseDocument = graphqlDocument + "ByVotes";
-    doTopReviewersTest(graphqlDocument, responseDocument, "VOTES", "MTM2MzU4OR8yODky");
+    doTopReviewersTest(graphqlDocument, responseDocument, "VOTES", "MTM2MzU4OR8yODky", null);
   }
 
-  void doTopReviewersTest(String graphqlDocument, String responseDocument, String sort, String cursor) throws IOException {
+  @Test
+  void shouldGetTopReviewersByVotesFor2000() throws IOException {
+    String graphqlDocument = "reviewer/topReviewers";
+    String responseDocument = graphqlDocument + "ByVotes2000";
+    doTopReviewersTest(graphqlDocument, responseDocument, "VOTES", "MTIyNDA4Nx82NQ==", 2000);
+  }
+
+  void doTopReviewersTest(String graphqlDocument, String responseDocument, String sort, String cursor, Integer year) throws IOException {
     Map<String, Object> params = new HashMap<>();
     params.put("limit", 5);
     params.put("cursor", cursor);
     params.put("sort", sort);
     tester
       .document(TestUtils.documentFor(graphqlDocument))
+      .variable("year", year)
       .variable("params", params)
       .execute()
       .path("$")
