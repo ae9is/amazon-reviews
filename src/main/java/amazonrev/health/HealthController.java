c6655033ddaddfee179e1dcd52b504e0086401a0
ae9is <125031666+ae9is@users.noreply.github.com>
2024-06-10 02:39:17 -0700

    refactor!: standardise api url versioning

--- src/main/java/amazonrev/health/HealthController.java
+++ src/main/java/amazonrev/health/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-  @GetMapping("/healthz")
+  @GetMapping("/v1/healthz")
   public String healthz() {
     return String.format("OK");
   }
