d08f56d6b34e881184936af7be323f9a2b78596c
ae9is <125031666+ae9is@users.noreply.github.com>
2024-06-11 06:25:31 -0700

    Recommendations model quantisation (#6)
    feat: support model quantisation and add env flag

--- src/main/python/amazonrev/embeddings.py
+++ src/main/python/amazonrev/embeddings.py
@@ -1,15 +1,19 @@
 import datetime as dt
 
 import torch
-from transformers import AutoModel, AutoTokenizer, RobertaModel, RobertaTokenizerFast
+from transformers import AutoModel, AutoTokenizer, RobertaModel, RobertaTokenizerFast, QuantoConfig
 
-from lib.config import MODEL_DIR
+from lib.config import MODEL_DIR, USE_QUANTIZATION
 from lib.logger import log
 
 
 log(f'Loading tokenizer and model at {MODEL_DIR} ...')
 tokenizer: RobertaTokenizerFast = AutoTokenizer.from_pretrained(MODEL_DIR)
-model: RobertaModel = AutoModel.from_pretrained(MODEL_DIR)
+quant_level = 'int4'
+quant_config: QuantoConfig = QuantoConfig(weights=quant_level) if USE_QUANTIZATION else None
+if USE_QUANTIZATION:
+  log(f'Using {quant_level} quantized model ...')
+model: RobertaModel = AutoModel.from_pretrained(MODEL_DIR, quantization_config=quant_config)
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 if device == 'cuda:0':
   log(f'Offloading model and tokenizer to {device} ...')
@@ -20,7 +24,7 @@ def generate_embeddings(texts: list[str]) -> list[float]:
   embeddings: torch.Tensor = None
   start = dt.datetime.now()
   log(f'Generating embeddings @ {start } ...')
-  inputs = tokenizer(texts, padding=True, truncation=True, max_length=512, return_tensors="pt").to(device=device)
+  inputs = tokenizer(texts, padding=True, truncation=True, max_length=512, return_tensors='pt').to(device=device)
   with torch.no_grad():
     embeddings = model(**inputs, return_dict=True).last_hidden_state[:, 0]  # Crashes silently here if too many inputs
     embeddings = embeddings / embeddings.norm(dim=1, keepdim=True)
